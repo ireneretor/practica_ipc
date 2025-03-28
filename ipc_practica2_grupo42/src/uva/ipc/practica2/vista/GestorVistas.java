9433ca9a32373a920881876455edf2d22bec5315
ireneretor <vader1504@gmail.com>
2025-03-28 09:31:33 +0100

    gestor vistas

---
+++ ipc_practica2_grupo42/src/uva/ipc/practica2/vista/GestorVistas.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uva.ipc.practica2.vista;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author irereto
+ */
+public class GestorVistas {
+
+    private JFrame vistaActual;
+
+    public void mostrarVistaMenuInicial() {
+        if (vistaActual != null) {
+            vistaActual.setVisible(false);
+            vistaActual.dispose();
+        }
+        vistaActual = new VistaLoginMenuInicial();
+        vistaActual.setVisible(true);
+    }
+
+    public void mostrarVistaGestorTareas() {
+        if (vistaActual != null) {
+            vistaActual.setVisible(false);
+            vistaActual.dispose();
+        }
+        vistaActual = new VistaGestorTareas();
+        vistaActual.setVisible(true);
+    }
+
+    public void mostrarVistaGestorListas() {
+        if (vistaActual != null) {
+            vistaActual.setVisible(false);
+            vistaActual.dispose();
+        }
+        vistaActual = new VistaGestorListas();
+        vistaActual.setVisible(true);
+    }
+
+}
